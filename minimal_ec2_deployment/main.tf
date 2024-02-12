5c8c770d132deacfc8e329c4e7897b43d5dee85a
Denis Ananev <DIAnaniev@gmail.com>
2024-02-12 21:10:03 +0200

    initial commit

--- minimal_ec2_deployment/main.tf
+++ minimal_ec2_deployment/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "lesson_03" {
+resource "aws_instance" "simple_instance" {
   ami           = "ami-0c7c4e3c6b4941f0f"
   instance_type = "t2.micro"
 
